139ce2685370a53614771bf824745084c44137cc
O-keita <o.keita@alustudent.com>
2023-10-30 19:23:09 +0200

    comit08

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming with Python\
+str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Print the desired part of the string followed by a new line
-print(str[39:78]+ '\n')
+str = str[39:67] + str[107:112] + str[0:6]
+print(str)
 
