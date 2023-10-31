654e921a117301ae6ec3e33671311ed2e917d547
O-keita <o.keita@alustudent.com>
2023-10-31 14:37:24 +0200

    pep8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-sentence = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(sentence[39:67] + sentence[107:112] + sentence[:6])
+
+str_ = "Python is an interpreted, interactive, object-oriented programming" \
+       " language that combines remarkable power with very clear syntax"
+
+print(str_[39:67] + str_[107:112] + str_[:6])
+
 
