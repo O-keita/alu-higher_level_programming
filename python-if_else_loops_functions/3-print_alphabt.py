03a438d645e0a1544b9cf4750b65b06934b83674
O-keita <o.keita@alustudent.com>
2023-11-01 16:07:16 +0200

    commit03

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char_code in range(ord("a"), ord("z") +1):
+for char_code in range(ord("a"), ord("z") + 1):
     char = chr(char_code)
     if char not in ("q", "e"):
-        print("{}".format(char), end = "")
+        print("{}".format(char), end="")
