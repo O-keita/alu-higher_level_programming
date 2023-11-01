db6e08f6590ac46b9c1ef176b0403edff13d7fd0
O-keita <o.keita@alustudent.com>
2023-11-01 21:22:24 +0200

    commit08

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-def uppercase(str):
-        upper = 65 >= ord(str) <= 90
-        print("{}".format(upper))
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+        else:
+            uppercase_char = char
+        print(uppercase_char, end='')
+    print()
+
+# Test the function
+input_string = "Hello, World!"
+uppercase(input_string)
+
