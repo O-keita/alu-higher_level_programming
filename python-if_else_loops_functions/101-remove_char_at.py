3986de80dc4ee396d8de829e0d5101599e2bab1a
O-keita <o.keita@alustudent.com>
2023-11-03 19:22:30 +0200

    commit101

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -3,4 +3,4 @@ def remove_char_at(s, n):
     if 0 <= n < len(s):
         return s[:n] + s[n+1:]
     else:
-        return s  # If n is out of range, return the original string
+        return s  # If n is out of range, return the original stringg
