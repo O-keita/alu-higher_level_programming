cfc227ba6bded7f2c7574e8a3d713d883b3f4e16
O-keita <o.keita@alustudent.com>
2023-11-03 18:59:26 +0200

    commit100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 for char_code in range(ord('z'), ord('a') - 1, -1):
     char = chr(char_code)
-    if char_code % 2 == 0:
+    if char_code % 2 == 1:
         char = char.upper() if char.islower() else char.lower()
     print("{}".format(char), end='')
 
 # Output: zYxWvUtSrQpOnMlKjIhGfEdCbA
-
