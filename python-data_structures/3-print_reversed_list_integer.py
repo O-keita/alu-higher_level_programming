2cfaaba1f5b2aaebc9a7aea08cdc9039adb8dacd
O-keita <o.keita@alustudent.com>
2023-11-06 19:57:27 +0200

    commit 03

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if len(my_list) <= 0:
-        return my_list
+    if my_list == None:
+        return None
     else:
          my_list.reverse()
          for item in my_list:
