f1a77b29118653f72c43bc5e9946a8bbc9ebe2d1
O-keita <o.keita@alustudent.com>
2023-11-06 21:29:04 +0200

    commit10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ new_list = []
 def divisible_by_2(my_list=[]):
     for item in my_list:
         if item % 2 == 0:
-            new_list.append(True)
+            return "{} is divisible by 2".format(item)
             
         else:
-            new_list.append(False)
+            return "{} is not divisible by 2".format(item)
