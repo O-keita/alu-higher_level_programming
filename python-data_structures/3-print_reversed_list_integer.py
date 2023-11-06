7abe2165b7027a576dc63141d4fb063e81f9fabe
O-keita <o.keita@alustudent.com>
2023-11-06 20:02:18 +0200

    commit 03

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
     else:
         my_list.reverse()
         for item in my_list:
-            print("{}".format(item))
+            print("{:d}".format(item))
