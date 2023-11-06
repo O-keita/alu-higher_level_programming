c029b914a601bd38253b01a81f5e845b363e7b5f
O-keita <o.keita@alustudent.com>
2023-11-06 22:41:26 +0200

    commit06

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in metrix:
+        for col in row:
+            print("{:d}".format(col), end='' if row != col[-1] else "")
+        print()
