7f3612f2dc67993e90c190ce77353df7edd4ab07
O-keita <o.keita@alustudent.com>
2023-11-06 22:20:39 +0200

    commit05

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    result = ""
     for char in my_string:
         if char != "C" and char != "c":
-            print(char, end="")
+            result += char
+    return result
