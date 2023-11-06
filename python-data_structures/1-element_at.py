d0d013a654aa090875ae35d49709b9d52d88f6d1
O-keita <o.keita@alustudent.com>
2023-11-06 19:01:23 +0200

    commit01

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -4,4 +4,4 @@ def element_at(my_list, idx):
     if 0 <= idx > len(my_list):
         return None
     else: 
-        return "Element {:d} is at index {:d}".format(idx, my_list[idx])
+        return "Element {:d} is at index {}".format(idx, my_list[idx])
