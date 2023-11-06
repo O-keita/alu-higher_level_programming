3f5d1c2ac352dffa9cb93d1878b8c77e0439db3a
O-keita <o.keita@alustudent.com>
2023-11-06 23:28:59 +0200

    commit08

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    string_len = len(sentence)
-    firt_char = sentence[0]
-    my_tuple = ()
-    if sentence == "":
-        my_tuple[0] = None
-        my_tuple[1] = string_len
-        return my_tuple
+    if len(sentence) == 0:
+        first_char = None
     else:
-        my_tuple[0] = string_len
-        my_tuple[0] = first_char
-        return my_tuple
+        first_char = sentence[0]
+    return(len(sentence), first_char)
+
+
 
