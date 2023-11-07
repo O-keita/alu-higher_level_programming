270e186d8e62e0f90572f79db4549627f6e0fe3b
O-keita <o.keita@alustudent.com>
2023-11-07 17:02:37 +0200

    commit001

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    index_search = my_list.index(search)
-    new_list = my_list[:]
-    if search in my_list:
-        new_list[index_search] = replace
-        return new_list
-    else:
-        return my_list
+    new_list =[]
+    for item in my_list:
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list
+    
