9cd475a302d6f59a87e0125a4f447c12a2c9366c
O-keita <o.keita@alustudent.com>
2023-11-07 16:53:53 +0200

    commit001

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     index_search = my_list.index(search)
-
     new_list = my_list[:]
-
-
-
-    new_list[index_search] = replace
-
-    return new_list
+    if search in my_list:
+        new_list[index_search] = replace
+        return new_list
+    else:
+        return my_list
