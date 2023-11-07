9f351a08eecc83a65acb7decb56b676286273bc6
O-keita <o.keita@alustudent.com>
2023-11-07 18:06:51 +0200

    commit04

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+        result = []
+
+    for items in set_1:
+        if items not in set_2:
+            result.append(items)
+    for items in set_2:
+        if items not in set_1:
+            result.append(items)
+    return result
