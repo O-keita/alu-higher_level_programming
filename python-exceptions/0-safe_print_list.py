8ef522f7cc9ddb3efec06d6e8d8ab3f903678b29
O-keita <o.keita@alustudent.com>
2023-11-08 17:37:11 +0200

    commit00

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num_list = my_list[:x]
+    count = 0
     try:
-        for num in num_lsit:
-            print("{}".format(num), end='')
-    except:
-        print("There is an error")
+        for i in range(x):
+            print(my_list[i], end='')
+            count += 0
+    except IndexError:
+            pass
+    finally:
+            print(count)
+    return count
