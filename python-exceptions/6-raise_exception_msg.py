79383feb75ff9a92b0264fc847cf3d41e89c6781
O-keita <o.keita@alustudent.com>
2023-11-08 20:36:38 +0200

    commit06

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-         raise TypeError("C is fun")
-     except TypeError:
-         print("C is fun")
+    raise NameError(message)
