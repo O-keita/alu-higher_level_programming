6e2d547dbec0ec44d4eed4eb2edf00a06ead9f5a
O-keita <o.keita@alustudent.com>
2023-11-08 19:05:43 +0200

    commit02

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    val = value == int
     try:
         print("{:d}".format(value))
     except:
-        print("{} is not an integer".format(value))
-    return val
+        return False
