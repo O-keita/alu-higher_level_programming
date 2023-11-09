873e7257ac9a81edb283427c0a2c269ac84472bd
O-keita <o.keita@alustudent.com>
2023-11-09 23:02:04 +0200

    commit12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,55 +1,16 @@
 #!/usr/bin/python3
-# Python program to convert Roman
-# Numerals to Numbers
+class py_solution:
+    def roman_to_int(self, s):
+        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        int_val = 0
+        if s is None or type(s) is not str:
+            return 0
+        else:
+         
+         for i in range(len(s)):
+            if i > 0 and rom_val[s[i]] > rom_val[s[i - 1]]:
+                int_val += rom_val[s[i]] - 2 * rom_val[s[i - 1]]
+            else:
+                int_val += rom_val[s[i]]
 
-# This function returns value of
-# each Roman symbol
-def value(r):
-	if (r == 'I'):
-		return 1
-	if (r == 'V'):
-		return 5
-	if (r == 'X'):
-		return 10
-	if (r == 'L'):
-		return 50
-	if (r == 'C'):
-		return 100
-	if (r == 'D'):
-		return 500
-	if (r == 'M'):
-		return 1000
-	return 0
-
-def roman_to_int(roman_string):
-	res = 0
-	i = 0
-
-	while (i < len(roman_string)):
-
-		# Getting value of symbol s[i]
-		s1 = value(roman_string[i])
-
-		if (i + 1 < len(str)):
-
-			# Getting value of symbol s[i + 1]
-			s2 = value(roman_string[i + 1])
-
-			# Comparing both values
-			if (s1 >= s2):
-
-				# Value of current symbol is greater
-				# or equal to the next symbol
-				res = res + s1
-				i = i + 1
-			else:
-
-				# Value of current symbol is greater
-				# or equal to the next symbol
-				res = res + s2 - s1
-				i = i + 2
-		else:
-			res = res + s1
-			i = i + 1
-
-	return res
+        return int_val
