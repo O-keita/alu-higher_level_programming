8b53dc4e84f8110175c7c6574e82cb85b3c3d104
O-keita <o.keita@alustudent.com>
2023-11-10 23:33:41 +0200

    commit102

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -2,12 +2,12 @@
 def magic_calculation(a, b):
     result = 0
     try:
-      for i in range(1, 3):
-        if i > a:
-          raise Exception('Too far')
-        result += (a ** b) / i
-    except Exception: 
-      # If an exception is raised, simply add a and b.
-      result = a + b
+        for i in range(1, 3):
+            if i > a:
+                raise Exception('Too far')
+            result += (a ** b) / i
+    except Exception:
+        # If an exception is raised, simply add a and b.
+        result = a + b
 
     return result
