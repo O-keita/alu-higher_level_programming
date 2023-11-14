0db114b5bec2b2b0948e2392191026772cc48010
O-keita <o.keita@alustudent.com>
2023-11-14 19:16:34 +0200

    commit00

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,4 +2,5 @@
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 class Sqaure:
     """This is a class that will have sqaure objects"""
-    pass
+    def __init__(self):
+        """ Initializationof the class variables"""
