c2deaafc1f76c183fd4c68d764cc238d2efe5b9f
O-keita <o.keita@alustudent.com>
2023-11-14 22:40:48 +0200

    commit04

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -21,9 +21,13 @@ class Square:
     @size.setter
     def size(self, new_size):
         """ changing the value"""
+        self.__size = new_size
 
-        if not isinstance(size, int):
+
+
+        if not isinstance(new_size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = new_sizie
+
+        self.__size = new_size
