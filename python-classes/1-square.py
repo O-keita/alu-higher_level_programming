aca10d2cc43e8de2d92caba6162f53dfeb248f9f
O-keita <o.keita@alustudent.com>
2023-11-14 19:42:58 +0200

    commit01

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining the class variable Sqaure"""
+
+
+class Sqaure:
+    """After defining we initialized the intances"""
+    def __init__(self, size):
+        """Making the size private"""
+        self.__size = size
