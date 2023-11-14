abb484627d7e3305cc2c48aa0be839e733db934e
O-keita <o.keita@alustudent.com>
2023-11-14 19:58:47 +0200

    commit01

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,10 +2,9 @@
 """python3 -c 'print(__import__("my_module").__doc__)'"""
 
 
-class Sqaure:
-    """'print(__import__("my_module").MyClass.__doc__)'"""
+class Square:
+    """We defined the class square"""
 
     def __init__(self, size):
-        """python3 -c 'print(__import__("my_module").my_function.__doc__)'"""
-        """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
+        """We are initializing it her"""
         self.__size = size
