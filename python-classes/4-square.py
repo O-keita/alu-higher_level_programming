a6911bbbbf1339815774be1afec1dacb9ee6adab
O-keita <o.keita@alustudent.com>
2023-11-14 22:37:59 +0200

    commit04

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,17 +5,10 @@
 class Square:
     """ initialize the instances"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            """ checking if size is integer"""
-
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-
-        """ actual Initialization"""
-
         self.__size = size
+
     def area(self):
+
         """calculating area"""
 
         return (self.__size * self.__size)
@@ -28,4 +21,9 @@ class Square:
     @size.setter
     def size(self, new_size):
         """ changing the value"""
-        self.__size = new_size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = new_sizie
