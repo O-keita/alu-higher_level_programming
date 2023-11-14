bdfd44825ed4b18204b6b6ad199fd97ab690a9ac
O-keita <o.keita@alustudent.com>
2023-11-14 20:26:37 +0200

    commit02

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,11 +6,11 @@ class Square:
     """Here we instantiate the variables below"""
 
     def __init__(self,size=0):
-        """ Here we initialize the variable size"""
+        """Here we initialize the variable size"""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
 
-        self.size = size
+        self._size = _Square__size
