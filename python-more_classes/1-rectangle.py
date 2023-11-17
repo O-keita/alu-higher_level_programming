d7b0a5414729f9afcfd5da4157530aca700d96bc
O-keita <o.keita@alustudent.com>
2023-11-17 23:00:43 +0200

    commit02

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,52 +1,39 @@
 #!/usr/bin/python3
-""" We are creating a Rectangle object that take w and h"""
+""" class for Rectangle"""
 
 
 class Rectangle:
-    """ We will initialize our private variables """
+    """Define a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """ the initialization"""
-
-        if not isinstance(width, int):
-            """ making sure width is an integer"""
-
-            raise TypeError("width must be an integer")
-
-        if not isinstance(height, int):
-            """ making sure height is an integer"""
-
-            raise TypeError("height must be an integer")
-
-        if height < 0:
-            raise ValueError("height must be >= 0")
-
-        if width < 0:
-            raise ValueError("width must be >= 0")
-
-        self.__width = width
-        self.__height = height
+        """Initialize width and height"""
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """ Getter for width"""
-
+        """Retrieve the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for width"""
-
+        """Set the width with type and value checking"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """ getter for height"""
-
+        """Retrieve the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Setter for height"""
-
+        """Set the height with type and value checking"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
