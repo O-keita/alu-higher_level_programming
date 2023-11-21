dbb59cb892eaa594df7484f2c82e40bae2a501d0
O-keita <o.keita@alustudent.com>
2023-11-21 16:02:19 +0200

    commit06

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -13,7 +13,7 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -60,4 +60,4 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
-        number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
