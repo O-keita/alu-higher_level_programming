e9c584f9887614df65ed691caf6346b4d7a21463
O-keita <o.keita@alustudent.com>
2023-11-21 17:52:03 +0200

    commit06

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -4,11 +4,11 @@
 
 class Rectangle:
     """ Initialization"""
+    number_of_instances = 0
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """ methods"""
-        number_of_instances = 0
-        print_symbol ="#"
 
         self.width = width
         self.height = height
