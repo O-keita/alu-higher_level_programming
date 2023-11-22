f388aaee571c5db88e8030a1887a49ec0db5f54b
O-keita <o.keita@alustudent.com>
2023-11-22 19:55:46 +0200

    commit01

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Sorted the object"""
+"""Defines an inherited list class MyList."""
 
 
 class MyList(list):
-    """ define the sorted function"""
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """Print a list in sorted ascending order."""
+        print(sorted(self))
