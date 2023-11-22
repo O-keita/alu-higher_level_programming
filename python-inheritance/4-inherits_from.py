b90f34026701301be0f2ad11367ad6365fe21926
O-keita <o.keita@alustudent.com>
2023-11-23 00:04:16 +0200

    commit04

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """ checking if obj is a subclass of a_class"""
 
-    if issubclass(obj, a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
