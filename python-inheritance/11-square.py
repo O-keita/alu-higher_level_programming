3050823c7449b5fe3c5cc9ea5b543880cbb0f9a1
O-keita <o.keita@alustudent.com>
2023-11-23 03:12:48 +0200

    commit11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Now lets deal with Square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ I hate documentation"""
+    def __init__(self, size):
+
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        string = "[" + self.__class__.__name__ + "] "
+        string += self.__size + "/" + self.__size
