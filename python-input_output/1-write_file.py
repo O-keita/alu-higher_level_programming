c3b2b8c9345ab8ec4c7a45153baa4d375f237f1e
O-keita <o.keita@alustudent.com>
2023-11-23 15:00:55 +0200

    commit01

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 1-write_file.py"""
+
+
+def write_file(filename="", text=""):
+    """ write the file with w"""
+
+    with open(filename, 'w', 'utf-8') as file:
+        words = file.write()
+
+        char = len(words)
+        return char
