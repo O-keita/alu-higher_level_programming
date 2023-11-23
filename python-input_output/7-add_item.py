178be3ccb5db9f9254e7e16b2943f66e5820310c
O-keita <o.keita@alustudent.com>
2023-11-23 18:03:33 +0200

    commit07

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,27 +1,15 @@
 #!/usr/bin/python3
-"""Json documentaion python3"""
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
-
-
-
-def main():
-    """ documentation is boring"""
-
-        items = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        items = []
-
-    # Add command line arguments to the list
-    items.extend(sys.argv[1:])
-
-    # Save the updated list to 'add_item.json'
-    save_to_json_file(items, 'add_item.json')
-
-    # Print the contents of the file
-    print("Contents of 'add_item.json':")
-    print(load_from_json_file('add_item.json'))
 
 if __name__ == "__main__":
-    main()
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
