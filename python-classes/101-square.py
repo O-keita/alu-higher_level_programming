dabb38d596747f4bcd1e596c821966f136052e09
O-keita <o.keita@alustudent.com>
2023-11-24 16:32:57 +0200

    commit101

--- python-classes/101-square.py
+++ python-classes/101-square.py
@@ -1,35 +1,85 @@
-#!/usr/bin/env python3
-"""
-MagicClass: Circle calculations.
+#!/usr/bin/python3
+""" module documented """
 
-Attributes:
-    __radius (float): The circle's radius.
 
-Methods:
-    __init__(self, radius): Initializes MagicClass with a given radius.
-    area(self): Returns the circle's area.
-    circumference(self): Returns the circle's circumference.
-"""
-
-import math
-
-class MagicClass:
-    def __init__(self, radius):
+class Square:
+    """ class documented """
+    def __init__(self, size=0, position=(0, 0)):
         """
-        Initializes MagicClass with a given radius.
+        Initializes a Square instance.
 
         Args:
-            radius (float): The circle's radius.
+            size (int): The size of the square (default is 0).
+            position (tuple): The position of the square (default is (0, 0)).
         """
-        self.__radius = 0
-        if type(radius) not in (int, float):
-            raise TypeError('radius must be a number')
-        self.__radius = radius
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter method for the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method for the size attribute.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter method for the position attribute."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Setter method for the position attribute.
+
+        Args:
+            value (tuple): The position of the square.
+
+        Raises:
+            TypeError: If position is not a tuple of 2 positive integers.
+        """
+        if (
+            type(value) is not tuple or
+            len(value) != 2 or
+            type(value[0]) is not int or
+            type(value[1]) is not int or
+            value[0] < 0 or
+            value[1] < 0
+        ):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
 
     def area(self):
-        """Returns the circle's area."""
-        return math.pi * self.__radius ** 2
+        """
+        Calculates and returns the area of the square.
 
-    def circumference(self):
-        """Returns the circle's circumference."""
-        return 2 * math.pi * self.__radius
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the character '#'."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
