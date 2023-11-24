0723d6733d7557fbc0b6c31e2f33eee9a45dcecf
O-keita <o.keita@alustudent.com>
2023-11-24 23:39:25 +0200

    commit103

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -15,36 +15,15 @@ Methods:
 import math
 
 class MagicClass:
-    """ class documented"""
+    """ module documented"""
     def __init__(self, radius):
-        """
-        Initializes MagicClass with a given radius.
-
-        Args:
-            radius (float): The circle's radius.
-        """
         self.__radius = 0
-        if type(radius) not in (int, float):
+        if type(radius) is not int or type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """Returns the circle's area."""
-        return math.pi * self.__radius ** 2
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Returns the circle's circumference."""
         return 2 * math.pi * self.__radius
-
-# Example instantiation and usage
-if __name__ == "__main__":
-    # Instantiate MagicClass with a radius value
-    circle = MagicClass(5.0)
-
-    # Example usage:
-    area_result = circle.area()
-    circumference_result = circle.circumference()
-
-    print(f"Radius: {circle._MagicClass__radius}")  # Accessing the private attribute
-    print(f"Area: {area_result}")
-    print(f"Circumference: {circumference_result}")
