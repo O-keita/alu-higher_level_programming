b26a0226093e26746ee8d80d345db369bffdeded
O-keita <o.keita@alustudent.com>
2023-12-01 00:01:44 +0200

    commit00

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Check if a URL is provided
-r=$(curl -sSL -w "%{http_code}" "$1"); [ "${r: -3}" -eq 200 ] && echo "${r::-3}" || echo "Error: HTTP status code ${r: -3}"
+r=$(curl -sSL -w "%{http_code}" "$1"); [ "${r: -3}" -eq 200 ] && echo -n "${r::-3}" || echo -n "Error: HTTP status code ${r: -3}"
