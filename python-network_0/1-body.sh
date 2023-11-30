2177058c4674443e7f870e0c4a358511906b1872
O-keita <o.keita@alustudent.com>
2023-11-30 23:52:02 +0200

    commit01

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
-# Will check later
-curl -fsSL "$1" | awk 'NR==1{if($2==200)print $0}'
+
+# Check if a URL is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Get the URL from the command line argument
+url="$1"
+
+# Use curl to fetch the URL and display the body for 200 status code
+response=$(curl -sSL -w "%{http_code}" "$url")
+
+# Extract the status code
+status_code=$(echo "$response" | tail -n1)
+
+# Check if the status code is 200
+if [ "$status_code" -eq 200 ]; then
+  # Display the response body
+  echo "Response Body:"
+  echo "$response" | head -n -1
+else
+  echo "Error: HTTP status code $status_code"
+fi
+
