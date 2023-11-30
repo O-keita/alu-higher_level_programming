a8662067dd6cba31c382d943c307c99901a7d569
O-keita <o.keita@alustudent.com>
2023-12-01 00:30:29 +0200

    commit00

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This networking sucks
-curl -sSL -H "X-HolbertonSchool-User-Id: 98" "$1"
+[ "$(curl -sSL -H "X-HolbertonSchool-User-Id: 98" "$1")" == "OK" ] && echo "OK" || echo "Error"
