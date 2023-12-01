72325eeb8dbff5940b54d2e0a87c16c48b87ff09
O-keita <o.keita@alustudent.com>
2023-12-01 21:30:46 +0200

    commit04

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
-"""
- Sends a request to the specified URL and displays the value of the X-Request-Id variable
-    in the response header.
-
-    Args:
-        url (str): The URL to send the request to.
-"""
+"""2-post_email.py"""
 
 
-import requests
+import urllib.request
+import urllib.parse
 import sys
-""" documented now"""
 
-def get_x_request_id(url):
-    """
-    Sends a request to the specified URL and displays the value of the X-Request-Id variable
-    in the response header.
+if len(sys.argv) != 3:
+    print("Usage: python script.py <URL> <email>")
+    sys.exit(1)
 
-    Args:
-        url (str): The URL to send the request to.
-    """
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx or 5xx)
+url = sys.argv[1]
+email = sys.argv[2]
 
-        # Display the value of the X-Request-Id variable in the response header
-        x_request_id = response.headers.get('X-Request-Id')
-        if x_request_id:
-            print(f"X-Request-Id: {x_request_id}")
-        else:
-            print("X-Request-Id not found in the response header.")
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
 
-if __name__ == "__main__":
-    # Replace this URL with the actual URL you want to test against
-    test_url = "http://example.com"
-
-    # Call the function with the provided URL
-    get_x_request_id(test_url)
+with urllib.request.urlopen(req) as response:
+    content = response.read().decode('utf-8')
+    print(content)
