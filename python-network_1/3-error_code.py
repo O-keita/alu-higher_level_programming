5c1eb9d1d77971aa56296ac34f172bd171011d4e
O-keita <o.keita@alustudent.com>
2023-12-01 21:02:41 +0200

    commit03

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""python3 -c 'print(__import__("my_module").__doc__)'"""
+"""2-post_email.py"""
 
 
 import urllib.request
-import urllib.error
+import urllib.parse
 import sys
 
-def fetch_url(url):
-    try:
-        # Open the URL and read the response
-        with urllib.request.urlopen(url) as response:
-            # Decode the response body using UTF-8 and print it
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-        # Handle HTTPError by printing the error code
-        print(f"Error code: {e.code}")
+if len(sys.argv) != 3:
+    print("Usage: python script.py <URL> <email>")
+    sys.exit(1)
 
-# Test the script with a URL (assuming the web server is running on port 5000)
-if __name__ == "__main__":
-    # Replace this URL with the actual URL you want to test against
-    test_url = "http://localhost:5000"
-    fetch_url(test_url)
+url = sys.argv[1]
+email = sys.argv[2]
+
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+req = urllib.request.Request(url, data=data, method='POST')
+
+with urllib.request.urlopen(req) as response:
+    content = response.read().decode('utf-8')
+    print(content)     
