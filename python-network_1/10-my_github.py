f956c98e0f883bd7d8477122043a3687ee6213a8
O-keita <o.keita@alustudent.com>
2023-12-01 17:55:33 +0200

    commit10

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Question 10"""
+
+
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: python script.py <username> <token>")
+    sys.exit(1)
+
+username = sys.argv[1]
+token = sys.argv[2]
+
+url = "https://api.github.com/user"
+
+response = requests.get(url, auth=(username, token))
+
+if response.status_code == 200:
+    user_data = response.json()
+    user_id = user_data.get('id')
+    print(f"Your GitHub user ID is: {user_id}")
+else:
+    print(f"Error: Unable to fetch user information. Status code: {response.status_code}")
+    print(response.text)
