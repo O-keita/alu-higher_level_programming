32394b4461f26df9aa6ab7af7274aaa387b44f27
O-keita <o.keita@alustudent.com>
2023-12-01 15:13:09 +0200

    commit04

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #This networking sucks
-curl -sSL -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -sSL -w "%{http_code}" -H "X-HolbertonSchool-User-Id: 98" "$1" | grep -q 200 && echo "OK" || echo "Request failed"
