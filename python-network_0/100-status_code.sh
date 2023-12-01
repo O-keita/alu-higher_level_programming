640ace3ddd26547f183d72b3405d165c161fc41f
O-keita <o.keita@alustudent.com>
2023-12-01 15:46:37 +0200

    commit100

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #The first advance question
-(status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1") && echo -n $status_code)
+(status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1"); printf "%s" "$status_code")
