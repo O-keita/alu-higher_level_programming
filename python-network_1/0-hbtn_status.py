2e7ee811b018f64822786ef662e832915381ff0e
O-keita <o.keita@alustudent.com>
2023-12-01 17:43:28 +0200

    commit00

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -7,8 +7,9 @@ import urllib.request
 url = "https://alu-intranet.hbtn.io/status"
 
 with urllib.request.urlopen(url) as response:
-    content = response.read().decode('utf-8')
+    content = response.read()
 
     print("Body response:")
     print("\t- type:", type(content))
     print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
