3d10c511fae8d75cd4136077ba32716641f1a0ea
O-keita <o.keita@alustudent.com>
2023-12-05 17:56:11 +0200

    commit03

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,8 +9,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    result = print(f"My name is {first_name} {last_name}")
-    return result
+    print(f"My name is {first_name} {last_name}")
 
 if __name__ == "__main__":
     import doctest
