cbf0b5b4385f646ffe1477f9d3070c970a1fe686
O-keita <o.keita@alustudent.com>
2023-12-05 17:37:59 +0200

    commit03

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Printing names """
+
+
+ def say_my_name(first_name, last_name=""):
+     """ The function will take first and last name"""
+
+     if not isinstance(first_name, str):
+         raise TypeError("first_name must be a string")
+     if not isinstance(last_name, str):
+         raise TypeError("last_name must be a string")
+     result = print(f"My name is {first_name} {last_name}")
+     return result
