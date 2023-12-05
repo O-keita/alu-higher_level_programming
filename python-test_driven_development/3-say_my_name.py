aecbc49eea2e88bb1aa4dac26213fa0dce7db271
O-keita <o.keita@alustudent.com>
2023-12-05 17:53:49 +0200

    commit03

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -11,3 +11,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
     result = print(f"My name is {first_name} {last_name}")
     return result
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
