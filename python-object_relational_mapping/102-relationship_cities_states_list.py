532c2759ebbc9155a17c6831fdf0bbbb1176e759
O-keita <o.keita@alustudent.com>
2023-12-14 07:48:12 +0200

    commit

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ prints the State object with the name passed as argument from the database
 """
+
 import sys
 from relationship_state import Base, State
 from relationship_city import City
@@ -10,6 +11,8 @@ from sqlalchemy.orm import relationship
 
 
 if __name__ == "__main__":
+    """ documented """
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
