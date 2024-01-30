8705d0635f1b7be0c124a7930a7c933a61590275
O-keita <o.keita@alustudent.com>
2024-01-30 11:09:52 +0200

    commit2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ const numberOfArguments = process.argv.length - 2;
 // Subtracting 2 to exclude 'node' and the script file path
 
 if (numberOfArguments === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (numberOfArguments === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
