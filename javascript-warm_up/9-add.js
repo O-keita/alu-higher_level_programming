ea8da42274a0d6eba1d1828d0393ad9b185c8630
O-keita <o.keita@alustudent.com>
2024-01-31 23:27:17 +0200

    commit9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,6 +3,6 @@ function add (a, b) {
   console.log(a + b);
 }
 
-const arg1 = process.argv[2]
-const arg2 = process.argv[3]
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 add(parseFloat(arg1), parseFloat(arg2));
