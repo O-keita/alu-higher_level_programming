26cf1429fd8f050098a2af2023c59c7b2c7cfd01
O-keita <o.keita@alustudent.com>
2024-02-03 16:21:13 +0200

    commit03

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 
-class Rectangle{
-    constructor(w, h){
-        if (w <=0 || h <=0 || !Number.isInteger(w) || !Number.isInteger(h)) return null;
-        this.width =w;
-        this.height = h;
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) return null;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let rec = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        rec = rec + 'X';
+      }
+
+      console.log(rec);
+      rec = '';
     }
-
-    print(){
-        let rec =''
-        for (let i= 0; i < this.width; i++){
-            for (let j= 0; j<this.height; j++){
-                rec = rec + "X"
-            }
-
-            console.log(rec) 
-            rec =''  
-        }
-
-    }
+  }
 }
 
-module.exports = Rectangle
+module.exports = Rectangle;
