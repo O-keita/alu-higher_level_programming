c7d7b711a4a06cedc89c6d1e755cd39b77fe5426
O-keita <o.keita@alustudent.com>
2024-02-03 16:16:40 +0200

    commit03

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle{
+    constructor(w, h){
+        if (w <=0 || h <=0 || !Number.isInteger(w) || !Number.isInteger(h)) return null;
+        this.width =w;
+        this.height = h;
+    }
+
+    print(){
+        let rec =''
+        for (let i= 0; i < this.width; i++){
+            for (let j= 0; j<this.height; j++){
+                rec = rec + "X"
+            }
+
+            console.log(rec) 
+            rec =''  
+        }
+
+    }
+}
+
+module.exports = Rectangle
