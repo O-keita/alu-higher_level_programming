baffcf90f803b6b18d7d2a9ae598f71506482ab8
O-keita <o.keita@alustudent.com>
2024-02-07 13:24:58 +0200

    commit0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const readFiles = (path) => {
+  try {
+    const content = fs.readFile(path, 'utf-8');
+
+    console.log(content);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (require.main === module) {
+  if (process.argv.length !== 3) {
+    console.log('Please Provide a file ');
+  } else {
+    const path = process.argv[2];
+    readFiles(path);
+  }
+}
