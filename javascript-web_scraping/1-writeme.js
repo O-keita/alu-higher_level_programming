a1c4cdadd0ec2ed331b09a9e35062a3b55770685
O-keita <o.keita@alustudent.com>
2024-02-07 14:09:48 +0200

    commit1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,28 +1,20 @@
 #!/usr/bin/node
-const fs = require('fs')
-const writeFile =(path, text)=>{
+const fs = require('fs');
+const writeFile = (path, text) => {
+  try {
+    fs.writeFileSync(path, text, 'utf-8');
+  } catch (error) {
+    console.log(`${error}`);
+  }
+};
 
-    try{
-        content = fs.writeFileSync(path, text, 'utf-8')
+if (require.main === module) {
+  if (process.argv.length !== 4) {
+    console.log(`You provide ${process.argv.length - 2} args out of 2, please provide all args`);
+  } else {
+    const path = process.argv[2];
+    const text = process.argv[3];
 
-    }
-
-    catch (error){
-        console.log(`${error}`)
-    }
-
-}
-
-if (require.main === module){
-
-    if (process.argv.length !== 4){
-        console.log(`You provide ${process.argv.length - 2} args out of 2, please provide all args`)
-    }
-
-    else{
-        const path = process.argv[2]
-        const text = process.argv[3]
-
-        writeFile(path, text)
-    }
+    writeFile(path, text);
+  }
 }
