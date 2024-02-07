561f27512715aba5fef8b22109c14da81447ec31
O-keita <o.keita@alustudent.com>
2024-02-07 16:41:01 +0200

    commit3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const printTitle = (movieId) => {
+  const url = 'https://swapi-api.alx-tools.com/api/films/:id';
+
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.log(`Error: ${error}`);
+    } else {
+      try {
+        const movie = JSON.parse(body);
+        console.log(`${movie.title}`);
+      } catch (parseError) {
+        console.log(parseError.message);
+      }
+    }
+  });
+};
+
+if (require.main === module) {
+  if (process.argv.length !== 3) {
+    console.log('Please provide movie ID');
+  } else {
+    const movieId = process.argv[2];
+
+    printTitle(movieId);
+  }
+}
